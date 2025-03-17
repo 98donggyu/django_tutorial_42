1d25a67b7ae8015d243aa64fc52df35a88b052e9
98donggyu <98donggyu@gmail.com>
2025-03-17 17:34:15 +0900

    community앱 작성한 글 목록 보기 구현

--- community/views.py
+++ community/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render
 from community.forms import Form
+from .models import Article
 
 # Create your views here.
 def write(request):
-    if request.method == 'Post':
+    if request.method == 'POST':
         form = Form(request.POST)
+        print(form)
         if form.is_valid():
             form.save() # 필드 값 저장하기
     else:
         form = Form()  
     return render(request, 'write.html', {'form' : form}) # request, template,
+
+def articleList(requests):
+    article_list = Article.objects.all()
+    print(article_list)
+    return render(requests, 'list.html', {'article_list' : article_list})
