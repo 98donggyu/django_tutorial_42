d416b7b5327affbd8265d40d34cfea3de5ff72a9
98donggyu <98donggyu@gmail.com>
2025-03-18 14:28:02 +0900

    index 페이지 구현

--- community/views.py
+++ community/views.py
@@ -22,3 +22,7 @@ def viewDetail(request, num=1):
     article_detail = Article.objects.get(id=num)
     # article_detail = get_object_or_404_(Article, id=num)
     return render(request, 'view_detail.html', {'article_detail' : article_detail})
+
+def index(request):
+    latest_article_list = Article.objects.all().order_by('-cdate')[:3]
+    return render(request, 'index.html', {'latest_article_list' : latest_article_list})
--- django_tutor/urls.py
+++ django_tutor/urls.py
@@ -16,11 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from community.views import write, articleList, viewDetail
+from community.views import write, articleList, viewDetail, index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('write/', write, name='write'), # path, view의 함수
     path('list/', articleList, name='list'),
-    path('view_detail/<int:num>/', viewDetail, name='view_detail')
+    path('view_detail/<int:num>/', viewDetail, name='view_detail'),
+    path('', index),
 ]
